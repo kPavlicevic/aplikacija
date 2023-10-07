b54b26abf1242d29b2820878981d727a845cf8ba
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-10-07 14:41:25 +0200

    frontend, wwwroot

--- csharp/Frontend/film-pwa/src/App.js
+++ csharp/Frontend/film-pwa/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
@@ -8,15 +9,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kata's Movie Database
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://imdb.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Enter
         </a>
       </header>
     </div>
---
+++ csharp/Frontend/film-pwa/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import App from '../App';
+
+export default function HomePage() {
+  return (
+    <>
+        <table>
+            <tr>
+                <th>Data</th>
+            </tr>
+            <
+        </table>
+    </>
+    );
+}
